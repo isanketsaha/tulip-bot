8431e20f0dcd9881e349f2b45dc65a0242e09f18
Sanket Saha <sanketsaha@gmail.com>
2025-04-13 12:50:29 +0530

    fixed the failure issue

--- src/handler.py
+++ src/handler.py
@@ -1,45 +1,41 @@
-import email
-import os
+import json
 import traceback
-from datetime import datetime, timedelta
-from email.message import EmailMessage
-from typing import Dict, Union, List
+from typing import Dict
 
 from src.daily_transaction import TransactionReportHandler
 from src.stock_alert import StockAnalysis
-from src.utils import aws_session, logger
+from src.utils import logger
 
 
-class Handle:
+def handle() -> Dict:
+    return {
+        'Daily_Transaction_Report': TransactionReportHandler,
+        'STOCK_SCAN': StockAnalysis
+    }
 
-    def handle_request(self, event: Dict, context: Dict):
-        try:
-            print("Event received")
-            print(event)
-            invoke = self.handle().get(event['ruleName'], lambda: "Invalid selection")()
-            invoke.handle_request()
-            return {
-                "statusCode": 200,
-                "body": "Emails Processed Successfully"
-            }
-        except Exception as e:
-            logger.error(f"Error processing email record: {traceback.format_exc()}")
-            return {
-                "statusCode": 404,
-                "body": "Errors while processing email"
-            }
 
-    def handle(self):
+def handle_request(event: Dict, context: Dict):
+    try:
+        event = json.loads(event)
+        print(f"Event value = {event['ruleName']}")
+        rules = handle()
+        invoke = rules.get(event['ruleName'], lambda: "Invalid selection")()
+        invoke.handle_request()
         return {
-            'Daily_Transaction_Report':  TransactionReportHandler,
-            'STOCK_SCAN': StockAnalysis
+            "statusCode": 200,
+            "body": "Emails Processed Successfully"
+        }
+    except Exception as e:
+        logger.error(f"Error processing email record: {traceback.format_exc()}")
+        return {
+            "statusCode": 404,
+            "body": "Errors while processing email"
         }
 
 
 def lambda_handler(event, context):
-    handler = Handle()
-    return handler.handle_request({'ruleName' : 'STOCK_SCAN'}, context)
+    return handle_request(event, context)
 
 
 if __name__ == "__main__":
-    lambda_handler("", "")
+    lambda_handler({'ruleName': 'Daily_Transaction_Report'}, "")
